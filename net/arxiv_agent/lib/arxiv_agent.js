82cf47784f3f68066debc28f7f250d28ee3a8d27
isamu <isamu@to-kyo.to>
2024-12-03 20:10:32 +0900

    build

---
+++ net/arxiv_agent/lib/arxiv_agent.js
@@ -0,0 +1,50 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.arxivAgent = void 0;
+const arXiv_api_ts_1 = __importDefault(require("arXiv-api-ts"));
+const arxivAgent = async ({ params, namedInputs, }) => {
+    const { searchQueryParams, sortBy, sortOrder, start, maxResults } = {
+        ...params,
+        ...namedInputs,
+    };
+    const papers = await (0, arXiv_api_ts_1.default)({
+        searchQueryParams,
+        sortBy,
+        sortOrder,
+        start,
+        maxResults,
+    });
+    return papers;
+};
+exports.arxivAgent = arxivAgent;
+const arxivAgentInfo = {
+    name: "arxivAgent",
+    agent: exports.arxivAgent,
+    mock: exports.arxivAgent,
+    samples: [
+        {
+            params: {
+                searchQueryParams: [
+                    {
+                        include: [{ name: "LLM" }],
+                    },
+                ],
+                sortBy: "lastUpdatedDate",
+                sortOrder: "descending",
+                start: 0,
+                maxResults: 100,
+            },
+            inputs: {},
+            result: {},
+        },
+    ],
+    description: "Arxiv Agent",
+    category: ["net"],
+    author: "Receptron team",
+    repository: "https://github.com/receptron/graphai-agents",
+    license: "MIT",
+};
+exports.default = arxivAgentInfo;
---
+++ net/arxiv_agent/lib/index.d.ts
@@ -0,0 +1,2 @@
+import arxivAgent from "./arxiv_agent";
+export { arxivAgent };
